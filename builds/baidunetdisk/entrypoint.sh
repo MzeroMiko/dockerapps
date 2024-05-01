f838b816ea742b158ea029d29fa1831ea7bb3baa
MzeroMiko <3496274007@qq.com>
2024-05-02 00:48:53 +0800

    update

--- builds/baidunetdisk/entrypoint.sh
+++ builds/baidunetdisk/entrypoint.sh
@@ -17,7 +17,8 @@ chown $PUID:$PGID -R /config $_HOME
 if [ ! -z $PASSWORD ]; then PASSWORD="-passwd $PASSWORD"; fi;     
 rm -r /tmp/.X* ; # rm /tmp/.X* to rm previous Xvfb running     
 Xvfb $DISPLAY -ac -screen 0 $RESOLUTION > /dev/null 2>&1 &     
-x11vnc -forever -repeat -loop -reopen -shared -listen 0.0.0.0 -rfbport 5900 $PASSWORD -display $DISPLAY > /dev/null 2>&1 &     
+x11vnc -forever -repeat -loop -reopen -shared -listen 0.0.0.0 -rfbport 5900 $PASSWORD -display $DISPLAY > /dev/null 2>&1 &  
+chmod 755 /novnc/wsnovnc   
 /novnc/wsnovnc -novncprefix $VNCPATH -novncroot /novnc/noVNC -bindaddr [::]:8000 -backaddr 127.0.0.1:5900 > /dev/null 2>&1 &     
 /novnc/wsnovnc -novncprefix $VNCPATH -novncroot /novnc/noVNC -bindaddr [::]:8001 -backaddr 127.0.0.1:5900 -tls -cert /config/ssl/$CERTFILE -key /config/ssl/$KEYFILE > /dev/null 2>&1 &     
 su $_UUSER -c /entrypoint_user.sh 

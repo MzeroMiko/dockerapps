12656da0a7f74c87b7251e8caf08262a067663e2
MzeroMiko <3496274007@qq.com>
2024-12-23 17:30:14 +0800

    add adminview

---
+++ builds/goserve/goserve/public/libs/libs/adminview.js
@@ -0,0 +1,309 @@
+"use strict"
+
+function html_to_shadow(module_html) {
+    let shadow_module = document.createElement('div')
+    shadow_module.attachShadow({mode: "open"})
+    shadow_module.shadowRoot.innerHTML = module_html
+    return shadow_module
+}
+
+function AdminViewer(opts = {}) {
+    let args = {
+        box: null,
+        html: opts.html,
+        params: {
+            adminCore: {}, popMenu: {}, iView: {}, PView: (opts)=>{ return {}},
+        },
+        styles: {
+            upFontColor: "#777", upProgColor: "#cce",
+        },
+    }
+    for (let key in opts) if (key in args.styles) args.styles[key] = opts[key];
+    for (let key in opts) if (key in args.params) args.params[key] = opts[key];
+
+    let shadow_module = html_to_shadow(args.html)
+    args.box.appendChild(shadow_module)
+    for (let key in args.styles) shadow_module.style.setProperty('--' + key, args.styles[key]);
+
+    let authTimeHandler;
+    let upBox = document.createElement("div");
+    let _upItemHtml = shadow_module.shadowRoot.querySelector(".padding")
+    _upItemHtml.remove()
+    let upItemHtml = _upItemHtml.innerHTML;
+    let moveBox = document.createElement("div");
+    let movePage = args.params.PView({ box: moveBox, colWidth: ["100%", "0%", "0%"], colmenu: false, });
+
+    function popTemplates(sign, finishCallBack) {
+        return {
+            authFail: (info) => { args.params.popMenu.appendMessage("fail", "Authorization Fail"); },
+            fail: (info) => { args.params.popMenu.appendMessage("fail", sign + " : fail " + info); },
+            exist: (info) => { args.params.popMenu.appendMessage("warn", sign + " : exist " + info); },
+            info: (info) => { args.params.popMenu.appendMessage("info", sign + " : get info " + info); },
+            pass: (info) => { args.params.popMenu.appendMessage("pass", sign + " : pass " + info); finishCallBack(); },
+            all: (info) => { },
+        }
+    }
+    function login(login = true, loginCallBack = () => { }, logoutCallBack = () => { }) {
+        let waitAuthTimeout = (waitTime) => {
+            if (!args.params.adminCore.getAuthStat()) { logoutCallBack(); clearTimeout(authTimeHandler); }
+            authTimeHandler = setTimeout(function () { waitAuthTimeout(waitTime); }, waitTime);
+        }
+
+        if (login) {
+            args.params.popMenu.appendAuth((name, key) => {
+                if (!(name + key))
+                    return false;
+                args.params.adminCore.askAuthCore(name + key,
+                    () => { waitAuthTimeout(2000); loginCallBack(); },
+                    () => { popTemplates("").authFail(); logoutCallBack(); }
+                );
+            });
+        } else {
+            args.params.adminCore.closeSessionCore(function () { logoutCallBack(); });
+        }
+    }
+    function download(chosenFiles = []) {
+        chosenFiles.slice(0).forEach((path, index) => {
+            setTimeout(() => { args.params.adminCore.download(path); }, 800 * index);
+        });
+    }
+    function mkdir(dirPath = "", finishCallBack = () => { }) {
+        args.params.popMenu.appendMessage("input", "New Directory", "", (name) => {
+            args.params.adminCore.mkdirCore(dirPath + "/" + name, popTemplates("mkdir " + name, finishCallBack));
+        });
+    }
+    function rename(chosenPath = [], finishCallBack = () => { }) {
+        chosenPath.slice(0).forEach((srcPath) => {
+            let srcName = srcPath.slice(srcPath.lastIndexOf("/") + 1);
+            args.params.popMenu.appendMessage("input", "rename " + srcName, srcName, (dstName) => {
+                let dstPath = srcPath.slice(0, srcPath.lastIndexOf("/") + 1) + dstName;
+                args.params.adminCore.renameCore(srcPath, dstPath, popTemplates("rename " + srcName + " to " + dstName, finishCallBack));
+            });
+        });
+    }
+    function archive(chosenPath = [], finishCallBack = () => { }) {
+        chosenPath.slice(0).forEach((path) => {
+            let name = path.slice(path.lastIndexOf("/") + 1);
+            args.params.popMenu.appendMessage("input", "Archive folder " + name.slice(0, 5) + "... to:", name + ".tgz", (dstName) => {
+                let suffix = dstName.slice(dstName.lastIndexOf(".") + 1);
+                let format = (suffix == "tgz") ? "targz" : "zip";
+                let dstPath = path.slice(0, path.lastIndexOf("/") + 1) + dstName;
+                args.params.adminCore.archiveCore(path, dstPath, format, popTemplates("archive " + name + " to " + dstPath, finishCallBack));
+            });
+        });
+    }
+    function remove(chosenPath = [], finishCallBack = () => { }) {
+        chosenPath.slice(0).forEach((path) => {
+            let name = path.slice(path.lastIndexOf("/") + 1);
+            args.params.popMenu.appendMessage("confirm", "remove " + name + "?", "", () => {
+                args.params.adminCore.removeCore(path, popTemplates("remove " + name, finishCallBack));
+            });
+        });
+    }
+    function upload(dirPath = "", finishCallBack = () => { }) {
+        let uploadFileCore = (file, callback) => { args.params.adminCore.uploadFile(file, dirPath, callback); }
+        let upItemList = upBox.querySelector(".itemList");
+        upItemList.onclick = (event) => { event.cancelBubble = true; };
+        upBox.querySelector(".blank").onclick = () => { appendFiles(); }
+
+        function appendFiles() {
+            let fileInput = document.createElement("input");
+            fileInput.type = "file";
+            fileInput.multiple = "multiple";
+            fileInput.onchange = function () {
+                let finput = this;
+
+                function formatSize(sizeB = 0) {
+                    // Cautions: >> is limited to 32bit signed int, 1<<31 
+                    let GB = 1 << 30, MB = 1 << 20, KB = 1 << 10;
+                    if (sizeB > GB)
+                        return (sizeB / GB).toFixed(2) + "G";
+                    else if (sizeB > MB)
+                        return (sizeB / MB).toFixed(2) + "M";
+                    else if (sizeB > KB)
+                        return (sizeB / KB).toFixed(2) + "K";
+                    else
+                        return sizeB.toString() + "B";
+                }
+
+                for (let i = 0; i < finput.files.length; i++) {
+                    let file = finput.files[i];
+                    let itemNode = document.createElement('div');
+                    itemNode.innerHTML = upItemHtml;
+                    itemNode = itemNode.firstChild;
+                    itemNode.carryFile = file;
+                    itemNode.onUpload = false;
+                    itemNode.uploadStop = false;
+                    itemNode.uploadFinish = false;
+                    itemNode.querySelector('.colname').innerText = itemNode.carryFile.name;
+                    itemNode.querySelector('.colsize').innerText = formatSize(itemNode.carryFile.size);
+                    itemNode.querySelector('.colstat').innerText = "wait";
+                    itemNode.querySelector('.colstat').onclick = function () {
+                        let node = this.parentNode.parentNode;
+                        if (node.onUpload)
+                            node.uploadStop = true;
+                        else
+                            node.remove();
+                    };
+                    upItemList.appendChild(itemNode);
+                }
+                finput.remove();
+            };
+            fileInput.click();
+        }
+
+        function doUpload() {
+            let tnode = upItemList.firstChild;
+            while (tnode) {
+                if (!tnode)
+                    break;
+                if (tnode.uploadFinish == false && tnode.onUpload == false)
+                    break;
+                tnode = tnode.nextSibling;
+            }
+            if (tnode) {
+                tnode.onUpload = true;
+                uploadFileCore(tnode.carryFile, uploadFileCallBack);
+            }
+
+            function uploadFileCallBack(progress, status) {
+                // status: md5 / upload / finish / exist / fail / stop; progress: -1 or [0-1] 
+                if (progress >= 0) {
+                    tnode.querySelector(".progress").style.width = (100 * progress).toFixed(2) + "%";
+                }
+                tnode.querySelector(".colstat").innerText = status;
+                if (status == "stop") {
+                    tnode.onUpload = false;
+                    tnode.uploadFinish = true;
+                    tnode.remove();
+                } else if (status == "finish" || status == "exist" || status == "fail") {
+                    tnode.onUpload = false;
+                    tnode.uploadFinish = true;
+                    tnode = tnode.nextSibling;
+                    while (tnode) {
+                        if (!tnode)
+                            break;
+                        if (tnode.uploadFinish == false)
+                            break;
+                        tnode = tnode.nextSibling;
+                    }
+                    if (tnode) {
+                        tnode.onUpload = true;
+                        uploadFileCore(tnode.carryFile, uploadFileCallBack);
+                    } else {
+                        finishCallBack();
+                    }
+                } // else md5 upload
+                return (tnode) ? tnode.uploadStop : false;
+            }
+        }
+
+        function reset() {
+            setTimeout(() => {
+                if (!upItemList.firstChild) {
+                    return;
+                } else if (upItemList.firstChild.onUpload == true) {
+                    upItemList.firstChild.uploadStop = true;
+                    reset();
+                } else {
+                    upItemList.firstChild.remove();
+                }
+            }, 20);
+        }
+
+        let tools = args.params.iView.draw(upBox, "upload to " + dirPath, ["down", "exit"]);
+        tools.exitBtn.onclick = () => { reset(); args.params.iView.hide("hide"); };
+        tools.downBtn.onclick = () => { doUpload(); };
+    }
+    function mkfile(dirPath = "", finishCallBack = () => { }) {
+        args.params.popMenu.appendMessage("input", "New File ", "", (name) => {
+            let plainHtml = '<div style="position:relative;width:100%;height:100%;padding:1em;outline:none; overflow:auto;" contenteditable="true"></div>';
+            let plainBox = htmltoElement(plainHtml);
+            let tools = args.params.iView.draw(plainBox, "write to " + name, ["down", "exit"]);
+            tools.exitBtn.onclick = () => { args.params.iView.hide("hide"); };
+            tools.downBtn.onclick = () => {
+                let content = plainBox.innerText;
+                args.params.adminCore.mkfileCore(dirPath + "/" + name, content, popTemplates("mkfile " + name, finishCallBack));
+                args.params.iView.hide("hide");
+            };
+        });
+    }
+    function moveto(_chosenPath = [], finishCallBack = () => { }) {
+        let openFolder = (path = "", isFolder = true) => {
+            if (!isFolder)
+                return;
+            args.params.adminCore.openFolder(path, false, (info) => {
+                tools.titleBar.innerText = "move to " + info.Path;
+                info.FileList = [];
+                movePage.updateInfo(info);
+            });
+        }
+
+        let chosenPath = _chosenPath.slice(0);
+        if (chosenPath.length == 0)
+            return;
+        let tools = args.params.iView.draw(moveBox, "move to ", ["down", "exit"]);
+        movePage.setArgs({ openPath: openFolder, });
+        tools.exitBtn.onclick = () => { args.params.iView.hide("hide"); };
+        tools.downBtn.onclick = () => {
+            let dirPath = tools.titleBar.innerText.slice(("move to ").length);
+            let names = chosenPath.map((path) => { return path.slice(path.lastIndexOf("/") + 1); });
+            args.params.popMenu.appendMessage("confirm", "move files:\n" + names.join("\n") + "\n to \"" + dirPath + "\"?", "", (inp) => {
+                chosenPath.forEach((path) => {
+                    let name = path.slice(path.lastIndexOf("/") + 1);
+                    let dstPath = dirPath + "/" + name;
+                    args.params.adminCore.renameCore(path, dstPath, popTemplates("move " + name + " to " + dirPath, finishCallBack));
+                });
+            });
+            args.params.iView.hide("hide");
+        };
+        openFolder();
+
+
+    }
+    function copyto(_chosenPath = [], finishCallBack = () => { }) {
+        let openFolder = (path = "", isFolder = true) => {
+            if (!isFolder)
+                return;
+            args.params.adminCore.openFolder(path, false, (info) => {
+                tools.titleBar.innerText = "copy to " + info.Path;
+                info.FileList = [];
+                movePage.updateInfo(info);
+            });
+        }
+        let chosenPath = _chosenPath.slice(0);
+        if (chosenPath.length == 0)
+            return;
+        let tools = args.params.iView.draw(moveBox, "copy to ", ["down", "exit"]);
+        movePage.setArgs({ openPath: openFolder, });
+        tools.exitBtn.onclick = () => { args.params.iView.hide("hide"); };
+        tools.downBtn.onclick = () => {
+            let dirPath = tools.titleBar.innerText.slice(("move to ").length);
+            let names = chosenPath.map((path) => { return path.slice(path.lastIndexOf("/") + 1); });
+            args.params.popMenu.appendMessage("confirm", "copy files:\n" + names.join("\n") + "\n to \"" + dirPath + "\"?", "", (inp) => {
+                chosenPath.forEach((path) => {
+                    let name = path.slice(path.lastIndexOf("/") + 1);
+                    let dstPath = dirPath + "/" + name;
+                    args.params.adminCore.copytoCore(path, dstPath, popTemplates("copy " + name + " to " + dirPath, finishCallBack));
+                });
+            });
+            args.params.iView.hide("hide");
+        };
+        openFolder();
+    }
+
+    return {
+        getArgs: () => { return args; },
+        setArgs: (opts) => { for (let key in opts) if (key in args.params) args.params[key] = opts[key]; },
+        login: login,
+        download: download,
+        upload: upload,
+        mkdir: mkdir,
+        mkfile: mkfile,
+        rename: rename,
+        archive: archive,
+        remove: remove,
+        moveto: moveto,
+        copyto: copyto,
+    }
+}

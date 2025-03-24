71b3f0bf69327207d4d90a4c79d347e1bebe507e
MzeroMiko <3496274007@qq.com>
2025-03-25 02:13:00 +0800

    use kasmvnc

--- builds/firefox/entrypoint_desktop.sh
+++ builds/firefox/entrypoint_desktop.sh
@@ -3,5 +3,6 @@
 mkdir -p $HOME/.vnc/;
 cp -rp /config/kasmvnc.yaml $HOME/.vnc/kasmvnc.yaml;
 echo -e "${PASSWORD}\n${PASSWORD}\n" | vncpasswd -u $(whoami) -w -r
+vncserver -noxstartup
 sleep 2 && fluxbox > /dev/null 2>&1 &     
-
+echo "flux start"
